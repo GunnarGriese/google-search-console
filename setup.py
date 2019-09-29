cac2d456e6ff2aab89c049e26fbf596f1e5e13cb
ggriese <gunnar@trakken.de>
2019-09-29 14:54:30 +0200

    changed version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="gsc_light",
-    version="0.0.8",
+    version="0.0.9",
     author="Gunnar Griese",
     author_email="gunnargriese@gmx.de",
     description="A small package for Google Search Console",
